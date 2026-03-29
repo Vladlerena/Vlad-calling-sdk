5eb1cb296d749529b22fbb4ef9de3243e0f9fd35
Vladimiro Lerena <vllerena@cisco.com>
2026-03-28 22:20:44 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const secondCallTimer = document.querySelector('#secondCallNotification #call-ti
 const profileDropDown = document.getElementById("myDropdown");
 const profileOnline = document.querySelector(".dropbtn #availability");
 
-let service_app_token = 'YTM4ODhmZTAtMzJhMi00NDk5LWE4ZDctZDE5OTIwNjU0MTgyNzdkYzJjNTMtZGUy_P0A1_cbbe27d4-1f60-43cb-819b-fd3749c66621'; // Add the service app account token here
+let service_app_token = 'M2RlYTE0M2EtYjE1MS00YjUyLWJhMDMtZjY1ZjEyYzA0MzdlZDkxNzkxYTgtYTQ1_P0A1_cbbe27d4-1f60-43cb-819b-fd3749c66621'; // Add the service app account token here
 // const refresh_token = '';
 
 class callNotificationElement {
@@ -131,7 +131,7 @@ async function getJweToken() {
   myHeaders.append("Authorization", `Bearer ${service_app_token}`);
 
   const payload =  JSON.stringify({
-    "calledNumber": "1008", // Place your call queue/hunt group/destination number here
+    "calledNumber": "6100", // Place your call queue/hunt group/destination number here
     "guestName": "polders" // Any guest name can be given
   });
 
